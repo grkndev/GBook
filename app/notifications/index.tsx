5060278f1259a9f88bf9f8dc3a456a038727a5f7
GweepCreative <gweepcreative@gmail.com>
2024-09-29 20:52:13 +0300

    Notification Screen

--- app/notifications/index.tsx
+++ app/notifications/index.tsx
@@ -1,10 +1,38 @@
 import Text from "@/components/Text";
-import { View } from "react-native";
+import Icon from "@/lib/Icons/icon";
+import { Link } from "expo-router";
+import { FlatList, Image, Pressable, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ProfileIndex() {
   return (
-    <View>
-      <Text>Sa</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="h-full">
+        <View className="flex flex-row justify-between items-center p-8 border-b border-zinc-200">
+          <Link href={"/user"}>
+            <Icon name="ChevronLeft" size={24} color="#000" />
+          </Link>
+          <Text className="font-bold text-2xl">Bildirimler</Text>
+          <View className="w-6 h-6" />
+        </View>
+        <FlatList
+          data={Array.from({ length: 20 })}
+          renderItem={({ index }) => <NotificationCard text={`${index+1}`} />}
+          keyExtractor={(_, index) => index.toString()}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function NotificationCard({ text }: { text: string }) {
+  return (
+    <Pressable className="flex flex-row justify-start items-center w-full p-4 border-b border-zinc-200 gap-2">
+      <Image
+        src="https://cdnqrmenu.s3.eu-west-1.amazonaws.com/grkn/me3.jpg"
+        className="w-12 h-12 rounded-full"
+      />
+      <Text>{text}</Text>
+    </Pressable>
   );
 }
